207d4c4ce4d3c09ae5028cb19a21b3361679a29c
Per Nergård <per.nergard@gmail.com>
2015-02-26 14:47:06 +0100

    Added color coding for not translated properties, possible to change folder via textbox and store in dds.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,35 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Nergard.EPi.Plugins.RebuildSpecifiedURLSegments")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Nergard.EPi.Plugins.XmlResourceManager")]
+[assembly: AssemblyDescription("Xml resource file editor exposed in EPiServer Admin UI")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Nergard.EPi.Plugins.RebuildSpecifiedURLSegments")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
+[assembly: AssemblyCompany("Per Nergard, Valdis Iljuconoks")]
+[assembly: AssemblyProduct("Nergard.EPi.Plugins.XmlResourceManager")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ebcc79fa-3e3a-4372-a3f0-53cd10c78675")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("ad42b4f0-3576-49f4-9631-d3f6f9145fa2")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
